105954269e5d10b8d0ca039bc0a5a587f46a7701
PujanAle <68309277+PujanAle@users.noreply.github.com>
2020-08-15 16:09:10 +1000

    CorporateOwner class extends Owner class

---
+++ src/Assignment1/CorporateOwner.java
@@ -0,0 +1,58 @@
+
+package Assignment1;
+
+/**
+ * @author pujan
+ * StudentId 12134829
+ * CorporateOwner class extends Owner class and 
+ * is used for the corporate motor vehicle owner
+ */
+public class CorporateOwner extends Owner{
+    
+    // Australian Business Number of corporate owner
+    private int australianBusinessNumber;
+
+    /**
+     * constructor
+     * @param fName first name of corporate owner
+     * @param lName last name of corporate owner
+     * @param lNumber license number of corporate owner
+     * @param addr address of corporate owner
+     * @param number phone number of corporate owner
+     * @param abn Australian Business Number of corporate owner
+     */
+    public CorporateOwner(String fName, String lName, int lNumber, String addr, String number, int abn){
+        
+        // pass fName, lName, lNumber, addr, number to superclass constructor
+        super(fName, lName, lNumber, addr, number);
+        
+        australianBusinessNumber = abn;
+    }
+    
+    /**
+     * setAustralianBusinessNumber method
+     * @param abn 
+     */
+    public void setAustralianBusinessNumber(int abn){
+        
+        australianBusinessNumber = abn;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int getAustralianBusinessNumber(){
+        
+        return australianBusinessNumber;
+    }
+    
+    @Override
+    public String toString(){
+        
+        return String.format(super.toString() + 
+                " Their Australian Business Number is %d.", australianBusinessNumber);
+    }
+    
+ // End of class
+}
